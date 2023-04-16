de88d50e52da387619ba7a38c4d5dfc4ebe7d479
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-04-16 21:28:29 +0600

    Generics comment added.

--- CSharpLearning/OOPs/Generics/Calculator.cs
+++ CSharpLearning/OOPs/Generics/Calculator.cs
@@ -4,6 +4,10 @@ public static class Calculator
 {
    // This is a generics method.
    // Here, T is a type.
+   // we are using generics for type independent or type safety.
+   // instead of using autoboxing and unboxing, we can use generics.
+   // Generics improves performance.
+ 
    
    public static bool AreEqual<T>(T a, T b)
    {
