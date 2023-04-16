713768c01a5e4413641d5f1e6902fba39eabeb23
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-04-16 23:27:40 +0600

    Added some generics constrains.

--- CSharpLearning/OOPs/Generics/NewCalculator.cs
+++ CSharpLearning/OOPs/Generics/NewCalculator.cs
@@ -1,7 +1,21 @@
 namespace CSharpLearning.OOPs.Generics;
 
 // Here, T is a type.
-public static class NewCalculator<T>
+/**
+ * There are 5 different types of constraints for generics.
+ *
+ *  where T : IComparable
+ *  where T : Product
+ *  where T : class
+ *  where T : new()
+ *  where T : struct
+ *
+ * Here, Product is a user defined class.
+ * Here, new() meaning, any object that has default constructor.
+ *
+ * This constraints can be skipped sometimes.
+ */
+public static class NewCalculator<T> where T : class
 {
     public static bool Calculate(T a, T b)
     {
