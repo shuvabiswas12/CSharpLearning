d412bc9d162802c137f7e2792447a9deb31824bb
shuvabiswas12 <shuvabiswas12@gmail.com>
2024-04-09 21:18:48 +0600

    added static constructor.

---
+++ CSharpLearning/OOPs/StaticConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test
+{
+    class StaticConstructor
+    {
+        /**
+         * Static constructor does not use for creating object of this class.
+         * Infact, there is no object creating of this class for Static constructor.
+         */
+        static StaticConstructor()
+        {
+            Console.WriteLine("Static constructor called.");
+        }
+
+        /**
+         * Whenever this static property gets called, 
+         * static constructor will gets called automatically before executing the static method.
+         */
+        public static void GetAll()
+        {
+            Console.WriteLine("Getting all.");
+        }
+    }
+}
